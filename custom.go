c9b6de1986cd663a22b50e33b34758448fcec792
xs <china.gdxs@gmail.com>
2023-12-26 10:40:35 +0800

    feature:增加自定义错误

---
+++ custom.go
@@ -0,0 +1,18 @@
+// Package errors
+// @file      : custom.go
+// @author    : china.gdxs@gmail.com
+// @time      : 2023/12/26 10:39
+// @Description:
+package errors
+
+type CustomErr struct {
+	msg string
+}
+
+func (e CustomErr) Error() string {
+	return e.msg
+}
+
+func NewValid(msg string) error {
+	return &CustomErr{msg: msg}
+}
