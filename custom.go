65a34339ed0d9c06fb38cfd417177763d475b97d
xs <china.gdxs@gmail.com>
2024-02-07 15:39:35 +0800

    feat: 新增err 自定义code

--- custom.go
+++ custom.go
@@ -5,9 +5,12 @@
 // @Description:
 package errors
 
+import "net/http"
+
 type CustomErr struct {
-	msg  string
-	code int
+	msg        string
+	code       int
+	httpStatus int
 }
 
 func (e CustomErr) Error() string {
@@ -17,14 +20,26 @@ func (e CustomErr) Error() string {
 func (e CustomErr) Code() int {
 	return e.code
 }
+func (e CustomErr) HttpStatus() int {
+	return e.httpStatus
+}
 
 func NewCustom(msg string) error {
-	return &CustomErr{msg: msg}
+	return &CustomErr{msg: msg, httpStatus: http.StatusOK}
 }
 
 func NewCustomCode(msg string, code int) error {
 	return &CustomErr{
-		msg:  msg,
-		code: code,
+		msg:        msg,
+		code:       code,
+		httpStatus: http.StatusOK,
+	}
+}
+
+func Custom(msg string, code int, httpStatus int) error {
+	return &CustomErr{
+		msg:        msg,
+		code:       code,
+		httpStatus: httpStatus,
 	}
 }
